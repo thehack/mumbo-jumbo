73edeb8c518e67850d80e0cb5f5b5a9b518a3f17
Tim Inman <tim.inman@agmd.org>
2009-08-22 18:12:21 -0700

    fixed famous jumbos to work naturally

--- guestbook.rb
+++ guestbook.rb
@@ -14,17 +14,7 @@ class Shout
   property :accuracy_score, Integer
   property :reach_score, Integer
   property :total_score, Integer
-end
-
-class Famous
-  include DataMapper::Resource
-  property :id, Serial
-  property :jumbo_name, String
-  property :clarity_score, Integer
-  property :brevity_score, Integer
-  property :accuracy_score, Integer
-  property :reach_score, Integer
-  property :total_score, Integer
+  property :famous, Boolean
 end
 
 # Make sure our template can use <%=h
@@ -36,26 +26,21 @@ end
 get '/' do
   # Just list all the shouts
   @shouts = Shout.all(:order => [:id.desc], :limit => 8)
-  @famous = Famous.all(:order => [:id.desc], :limit => 8)
- @scores = Shout.all(:order => [:total_score.desc], :limit => 8)
+  @famous = Shout.all(:famous => true, :order => [:id.desc], :limit => 8)
+ 	@scores = Shout.all(:order => [:total_score.desc], :limit => 8)
   erb :index
 end
 
-get '/*/famous' do
-	@celeb = Famous.get(params[:'splat'])
-  @famous = Famous.all(:order => [:id.desc], :limit => 8)
-  @shouts = Shout.all(:order => [:id.desc], :limit => 8)
-	erb :showfamous
-end
-
 get '/*/show' do
-  @famous = Famous.all(:order => [:id.desc], :limit => 8)
+  @famous = Shout.all(:famous => true, :order => [:id.desc], :limit => 8)
   @shouts = Shout.all(:order => [:id.desc], :limit => 8)
 	@shout = Shout.get(params[:'splat'])
+	@scores = Shout.all(:order => [:total_score.desc], :limit => 8)
 	erb :show
 end
 
 get '/admin/add_famous_people' do
+	  @shouts = Shout.all(:order => [:jumbo_name.desc])
 	erb :add_famous
 end
 
@@ -101,46 +86,15 @@ post '/newmumbo' do
 redirect '/'+shout.id.to_s+'/show'
 end
 
-post '/newfamous' do
-	#defining some vars so we can use them in the model
-	message = params[:message].downcase
-  sentences = message.split(/[^a-zA-Z\,\;\"\'\-\_\(\)\*\%\^\$\$\@\~\+\=\{\}\[\]\:\<\>\s]/).length
-  words = message.split(" ")
-  wordnum = words.length
-  letters = message.chomp.length
-  buzzwords = %w[actionable  assessment benchmark change coach compensation actions resolution constraints competencies practices dashboard deliverables diagnosis downsize enterprise excellence gatekeeper geographically dispersed headhunter  income pressures individual contributor leadership learning experience hornet mastery matrix organization momentum    nesting outcomes partnership positive momentum practical application process product service environment recommendation reengineer requirements revenue rightsize sigma standards superior performance supply chain synergy system teamwork touchpoints]
-  selfish_words = %w[me myself i mine my]
-  selfish = words - (words - %w[me myself i mine my])
-  superlatives = (((words - ( words - %w[super best better most very really amazing awesome worst hate love nobody everybody always never honestly])).length)*2)/wordnum
-  clarity_metric = {3 => 15, 4 => 20, 5 => 25, 6 => 20, 7 =>15, 8 =>10}
-  brevity_metric = {0 => 0, 1 => 0, 2 => 5, 3 => 10, 4 => 10, 5 => 15, 6 => 15, 7 => 20, 8 => 20, 9 => 25, 10 => 25, 11 => 25, 12 => 20, 13 => 20, 14 => 20, 15 => 15, 16 => 15, 17 => 10, 18 => 10, 19 => 10, 20 => 10, 21 =>5, 22 =>5, 23 =>5, 24 =>5}
-	lpw = (letters/wordnum).round
-	wps = (wordnum/sentences).round
-	r_score = 25 - (((words - (words - (buzzwords + selfish_words))).length)*25)/wordnum
-	a_score = 25 - superlatives*5/wordnum
-	c_score = if lpw <= 2 || lpw >= 9
-    		5
-    	else 
-    	clarity_metric[lpw]
-    	end
-  	b_score = if wps > 24
-    		0
-    	else
-    		brevity_metric[wps]
-    	end
-  famous = Famous.create(
-		:jumbo_name => params[:jumbo_name].downcase,
-    :accuracy_score => a_score,
-    :reach_score => r_score ,
-    :clarity_score => c_score,
-    :brevity_score => b_score,
-    :total_score => a_score + b_score + c_score + r_score
-    )
-  redirect '/'
-end
-
 post '/shouts/*/delete' do
 	@shout = Shout.get(params['splat'])
 	@shout.destroy
+	redirect '/'
+end
+
+post '/shouts/*/makefamous' do
+	@shout = Shout.get(params['splat'])
+	@shout.famous = true
+	@shout.save
 	redirect '/'
 end
